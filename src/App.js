80f5ac8fca94a7bfd558d65257773760006a4788
abrar997 <abraralrawi997.com>
2022-09-16 02:30:18 +0300

    home done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Footer } from "./components";
-import {Home} from "./pages";
-
+import { Home } from "./pages";
+import { RiseLoader } from "react-spinners";
 const App = () => {
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/">
-            <Route index element={<Home />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <>
+      {loading ? (
+        <div className="flex justify-center items-center pt-[20%]">
+          <RiseLoader
+            color="#7329ac"
+            loading={loading}
+            margin="30%%"
+            size={40}
+            speedMultiplier={1}
+
+          />
+        </div>
+      ) : (
+        <div className="h-screen bg-white">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/">
+                <Route index element={<Home />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </div>
+      )}
+    </>
   );
 };
 
